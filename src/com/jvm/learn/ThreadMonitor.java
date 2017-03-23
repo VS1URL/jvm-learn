0bae9c9550d70271a5e64a7c013f837428d1aeb6
zwChen <zwchen_2012@163.com>
2017-03-23 23:29:49 +0800

    线程等待监控

---
+++ src/com/jvm/learn/ThreadMonitor.java
@@ -0,0 +1,50 @@
+package com.jvm.learn;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by zwchen on 17-3-23.
+ */
+public class ThreadMonitor {
+    /**
+     * 线程死循环演示
+     */
+    public static void createBusyThread(){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){}
+            }
+        }, "testBusyThread");
+        thread.start();
+    }
+
+    /**
+     * 线程等待演示
+     */
+    public static void createLockThread(final Object lock){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock){
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "testLockThread");
+        thread.start();
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        reader.readLine();
+        createBusyThread();
+        reader.readLine();
+        Object obj = new Object();
+        createLockThread(obj);
+    }
+}
