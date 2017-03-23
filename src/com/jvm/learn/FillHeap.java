5a85a448634e9dd53fa28895eecb05ccac2a86d9
zwChen <zwchen_2012@163.com>
2017-03-23 08:57:17 +0800

    jconsole 内存监控

---
+++ src/com/jvm/learn/FillHeap.java
@@ -0,0 +1,28 @@
+package com.jvm.learn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Options: -Xmx100M -Xms100M -XX:+PrintGCDetails -XX:UseSerialGC
+ * Created by zwchen on 17-3-22.
+ */
+public class FillHeap {
+
+    static class OOMObject{
+        public byte[] placeholder = new byte[64 * 1024 * 1024];
+    }
+
+    public static void fillHead() throws InterruptedException{
+        List<HeapOOM.OOMObject> list = new ArrayList<>();
+        for (int i = 0; i < 5000; i++) {
+            Thread.sleep(20L);
+            list.add(new HeapOOM.OOMObject());
+        }
+        System.gc();
+    }
+
+    public static void main(String[] agrs) throws InterruptedException {
+        fillHead();
+    }
+}
